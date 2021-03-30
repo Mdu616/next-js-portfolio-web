5fae899e2f7cc5b2d57e18ff4fe9c72586cb9848
Mdu616 <55834790+Mdu616@users.noreply.github.com>
2021-03-30 15:54:44 +0200

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -16,11 +16,11 @@ function Contact() {
 			    <div className="text-wrapper w-full">
 			      <h1 className="title">Contact</h1>
 			      <p className="description">
-			      	Incididunt in cillum magna occaecat nisi qui in exercitation labore anim adipisicing amet irure tempor.
+			      	Contact me and let's work together.
 			      </p>
 			      <ul className="contact-links">
-			      	<li className="contact-item">Email: lutfhi@me.com</li>
-			      	<li className="contact-item">Phone: +6212344321007</li>
+			      	<li className="contact-item">Email: mduduzi@isanode.com</li>
+			      	<li className="contact-item">Phone: +27 71 799 6756</li>
 			      </ul>
 		      </div>
 	      </div>
